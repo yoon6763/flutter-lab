6e254c63ae53d0da5f44d4ffceca756c0e044efd
Yoon <yoon6763@naver.com>
2022-02-09 17:10:33 +0900

    init

---
+++ magic-8-ball-flutter-master/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.blueAccent,
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+            backgroundColor: Colors.blue,
+          ),
+          body: ,
+        ),
+      ),
+    );
+
+class BodyPart extends StatefulWidget {
+  @override
+  _BodyPartState createState() => _BodyPartState();
+}
+
+class _BodyPartState extends State<BodyPart> {
+  @override
+  Widget build(BuildContext context) {
+    child:
+  }
+}
