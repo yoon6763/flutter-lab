8f48607e2829fbb53f404e3ac36bc29832b517a2
Yoon <yoon6763@naver.com>
2022-02-10 11:37:18 +0900

    selection9 practice

--- magic-8-ball-flutter-master/ios/Flutter/flutter_export_environment.sh
+++ magic-8-ball-flutter-master/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\USER\Downloads\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\USER\Downloads\magic-8-ball-flutter-master"
+export "FLUTTER_APPLICATION_PATH=C:\Users\USER\Downloads\flutter_practice\magic-8-ball-flutter-master"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
