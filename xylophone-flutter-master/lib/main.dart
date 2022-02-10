4904fa0bfd104ac9d78a7fcdbaec3f195923783d
Yoon <yoon6763@naver.com>
2022-02-10 16:01:39 +0900

    section 9 finish

--- xylophone-flutter-master/lib/main.dart
+++ xylophone-flutter-master/lib/main.dart
@@ -4,17 +4,47 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  var colorList = [
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.green,
+    Colors.teal,
+    Colors.blue,
+    Colors.purple
+  ];
+
+  void playSound(int idx) => new AudioCache().play('note$idx.wav');
+
+  Widget buildKey(int idx) {
+    return Expanded(
+      child: FlatButton(
+        color: colorList[idx-1],
+        onPressed: () {
+          playSound(idx);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-            child: Center(
-                child: FlatButton(onPressed: () {
-                  final player = AudioCache();
-                  player.play('note1.wav');
-
-                }, child: Text('Click Me')))),
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            buildKey(1),
+            buildKey(2),
+            buildKey(3),
+            buildKey(4),
+            buildKey(5),
+            buildKey(6),
+            buildKey(7),
+          ],
+        )),
       ),
     );
   }
