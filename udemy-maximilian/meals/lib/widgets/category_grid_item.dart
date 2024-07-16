7fa2b542b2551547324127dbcadbacadb4da7541
uknow <yoon6763@naver.com>
2024-07-16 15:28:12 +0900

    Meals App Finish

--- udemy-maximilian/meals/lib/widgets/category_grid_item.dart
+++ udemy-maximilian/meals/lib/widgets/category_grid_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:meals/models/category.dart';
 
 class CategoryGridItem extends StatelessWidget {
@@ -32,8 +33,8 @@ class CategoryGridItem extends StatelessWidget {
         child: Text(
           category.title,
           style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                color: Theme.of(context).colorScheme.onBackground,
-              ),
+            color: Theme.of(context).colorScheme.onBackground,
+          ),
         ),
       ),
     );
